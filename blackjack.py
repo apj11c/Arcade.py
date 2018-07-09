3aa9c01015f1a959276e6d04bcf0a65da77c692d
Gabe Siewert <gls15@my.fsu.edu>
2018-07-09 11:05:19 -0400

    Added blackjack and roulette games without GUI (will be finished once GUI component is complete)

--- blackjack.py
+++ blackjack.py
@@ -12,38 +12,192 @@
 		- played with one deck of cards
 		- score is kept for both user and dealer
 
+		- numeric cards (2-10) are worth their number
+		- face cards are worth 10
+		- aces are worth 1 or 11, whichever is most helpful
+
 		- if the player's hand exceeds 21, it is a loss
 		- elif the dealer's hand exceeds 21, it is a win
 		- elif the player's hand is equal to the dealer's hand, it is a tie
 		- elif the player's hand is greater than the dealer's hand, it is a win
 		- else it is a loss
-		
-		- if the round was a tie, ties += 1 for dealer and player
-		- elif the winner's hand was exactly 21,
-		  wins += 2 for the winner
-		- else wins += 1 for winner, losses += 1 for loser
 
-		- potential for increased difficulty level:
-			- play with > 1 deck of cards
-			- 'regular' mode where dealer chooses hit/stand randomly and
-			  'advanced' mode where dealer is smarter about hit/stand
+
+	remaining implementation
+	------------------------
+	
+		- will use PyQt5 to display a green casino table background as
+		  well as each card that the player has/all the cards at the end
+		  of a game
+		- will display wins/losses/ties/total number of games on the side,
+		  and update values as the games go on
+		- once player quits game, will display "Congratulations!" or
+		  "Better luck next time!" depending on the win/loss ratio
+
 '''
 
-# import PyQt4 packages
+# import PyQt5 packages
 import random
-
 
 # ------ gameplay, without graphics ------
 
-dealerWins = 0; dealerLosses = 0; dealerTies = 0
-userWins = 0; userLosses = 0; userTies = 0
 
-# main loop (until 'quit game' is selected)
-# user plays against the house/dealer (i.e. only one opponent)
-	# deal user two random cards (random.randint(1, numDecks * 52))
-	# ask user if they want another card ("hit me"/"stand" buttons)
-	# house makes either random or smart choice about being dealt another card
-	# when both the user and the house pass ("stand"), the round is over
-	# dealer's cards are revealed
-	# increment wins/losses/ties accordingly
+# Calculate total of cards
+def score(cards):
+	sum = 0
+	aceCount = 0
+	
+	for i in cards:
+		if i[0] == "A":
+			aceCount += 1
+		elif str.isdigit(i[0]):
+			if i[0] == "1":
+				sum += 10
+			else:
+				sum += int(i[0])
+		else:
+			sum += 10
+
+	for i in range(aceCount):
+		sum += 1
+
+	for i in range(aceCount):
+		if sum + 10 <= 21:
+			sum += 10
+
+	return sum
+
+
+# Deal cards to dealer and player
+def deal():
+	deck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS",
+			"QS", "KS", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H",
+			"10H", "JH", "QH", "KH", "AC", "2C", "3C", "4C", "5C", "6C", "7C",
+			"8C", "9C", "10C", "JC", "QC", "KC", "AD", "2D", "3D", "4D", "5D",
+			"6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+
+	playerCards = []
+	dealerCards = []
+	playerCards.append(deck[random.randint(0, len(deck) - 1)])
+	deck.remove(playerCards[0])
+	dealerCards.append(deck[random.randint(0, len(deck) - 1)])
+	deck.remove(dealerCards[0])
+	playerCards.append(deck[random.randint(0, len(deck) - 1)])
+	deck.remove(playerCards[1])
+	dealerCards.append(deck[random.randint(0, len(deck) - 1)])
+	deck.remove(dealerCards[1])
+
+	return deck, dealerCards, playerCards
+
+
+# Select regular or advanced mode
+#	- in regular mode, dealer randomly chooses to hit or stand
+# 	- in advanced mode, dealer strategically chooses to hit or stand
+mode = int(input("Select a mode:\n0 - Regular\n1 - Advanced\n> "))
+
+# Accounting for invalid input
+while mode != 0 and mode != 1:
+	mode = int(input("Invalid input. Please enter 0 or 1: "))
+
+choice = "0"
+wins = 0; losses = 0; ties = 0
+
+while choice.lower() != "n":
+
+	pIndex = 2
+	dIndex = 2
+
+	deck, dCards, pCards = deal()
+
+	gameNotOver = True
+	rnd = 1
+
+	while gameNotOver:
+		# If in first round or if hit chosen
+		if choice.lower() != "s":
+			print("\nYour cards: ", end="")
+			for i in pCards:
+				print(i + " ", end="")
+			print()
+
+			choice = input("Hit or stand (H/S)? ")
+
+			# Accounting for invalid input
+			while choice.lower() != "s" and choice.lower() != "h":
+				choice = input("Invalid input. Enter H or S: ")
+
+		# If hit chosen, give player additional card
+		if choice.lower() == "h":
+			pCards.append(deck[random.randint(0, len(deck) - 1)])
+			deck.remove(pCards[pIndex])
+			pIndex += 1
+
+		# If in regular mode and stand was not already randomly chosen,
+		# randomly choose between hit and stand
+		if mode == 0 and rnd == 1:
+			rnd = random.randint(0, 1)
+
+		# If in regular mode and hit was randomly chosen or if in
+		# advanced mode and hit is strategically beneficial, give
+		# dealer additional card
+		if (mode == 0 and rnd == 1) or (score(dCards) < 17 and mode == 1):
+			dCards.append(deck[random.randint(0, len(deck) - 1)])
+			deck.remove(dCards[dIndex])
+			dIndex += 1
+		# If dealer was not given additional card, the dealer "picked" stand.
+		# If the player also chose stand, the game is over
+		else:
+			if choice.lower() == "s":
+				gameNotOver = False
+
+		# If either player exceeds 21, the game is over	
+		if score(pCards) > 21 or score(dCards) > 21:
+			gameNotOver = False
+
+	
+	print("\nYour cards: ", end="")
+	for i in pCards:
+		print(i + " ", end="")
+	print("\nDealer's cards: ", end="")
+	for i in dCards:
+		print(i + " ", end="")
+	print()
+
+	# Calculate and print card totals
+	pScore = score(pCards)
+	dScore = score(dCards)
+
+	print("You got: " + str(pScore))
+	print("The dealer got: " + str(dScore))
+
+	# Adjust number of wins/losses/ties accordingly
+	if pScore > 21:
+		print("You lost!")
+		losses += 1
+	elif pScore > dScore or dScore > 21:
+		print("You won!")
+		wins += 1
+	elif pScore < dScore:
+		print("You lost!")
+		losses += 1
+	elif pScore == dScore:
+		print("You tied!")
+		ties += 1
+
+
+	choice = input("Continue (Y/N)? ")
+
+	# Accounting for invalid input
+	while choice.lower() != "y" and choice.lower() != "n":
+		choice = input("Invalid input. Please enter Y or N: ")
+
+
+# Print win/loss/tie stats, end game
+print("\nWins\tLosses\tTies   | Total")
+total = wins + losses + ties
+print("-----------------------|------")
+print(str(wins) + "\t" + str(losses) + "\t" + str(ties) + "      | " + str(total))
+
+
+print("\nThanks for playing!")
 
--- roulette.py
+++ roulette.py
@@ -26,9 +26,8 @@
 	
 	- the user will begin with an amount of money (say, $10,000) and can
 	  place bets (on 1, 2, 3, 4, 5, or 6 numbers, with  payouts of 35 to 1,
-	  17 to 1, 11 to 1, 8 to 1, 6 to 1, and 5 to 1, respectively, OR a bet
-	  on the first, middle, or last twelve numbers with a payout of 2 to 1)
-	- the user can play until they lose all of their money (<= 0) or until
+	  17 to 1, 11 to 1, 8 to 1, 6 to 1, and 5 to 1, respectively
+	- the user can play until they lose all of their money or until
 	  they decide to quit
 	- at the end of the game, whether they lost or won will be displayed
 	  before the window closes
@@ -38,17 +37,106 @@
 		- obviously, if the user loses all of their money before quitting,
 		  they lost
 
+	
+	Remaining Implementation
+	------------------------
+	
+	- Will use PyQt5 to display roulette wheel
+	- Will keep track of winnings on the side, update value as the game goes on
+	- Will display numbers bet on and result of spin as well	
+
 '''
 
-# import PyQt4 packages
-import random
-
+# import PyQt5 packages
+import random 
 
 # ------ gameplay, without graphics ------
 
-# main loop (until 'quit game' is selected OR user loses all their money)
-	# user selects the type of bet they want to make
-	# user selects how much they want to bet
-	# wheel is spun (random.randint(1, 38))
-	# determine whether user won the bet
-	# adjust the amount of money the user has accordingly
+wheel = {0 : "0", 1 : "28", 2 : "9", 3 : "26", 4 : "30", 5 : "11",
+		 6 : "7", 7 : "20", 8 : "32", 9 : "17", 10 : "5", 11 : "22",
+		 12 : "34", 13 : "15", 14 : "3", 15 : "24", 16 : "36", 17 : "13",
+		 18 : "1", 19 : "00", 20 : "27", 21 : "10", 22 : "25", 23 : "29",
+		 24 : "12", 25 : "8", 26 : "19", 27 : "31", 28 : "18", 29 : "6",
+		 30 : "21", 31 : "33", 32 : "16", 33 : "4", 34 : "23", 35 : "35",
+		 36 : "14", 37 : "2"}
+
+
+# Player starts with $10,000
+money = 10000
+
+# Respective returns on betting on 1, 2, 3, 4, 5, or 6 numbers
+odds = [35, 17, 11, 8, 6, 5]
+
+print("Current Balance: $" + str(money))
+
+choice = int(input("How many numbers do you want to bet on (1-6, 0 to quit)? "))
+
+# Accounting for invalid input
+while choice < 0 or choice > 6:
+	choice = int(input("Please enter a number 1-6 (0 to quit): "))
+
+
+# If 0 is chosen, quit game
+while choice != 0:
+	betAmt = int(input("How much do you want to bet? "))
+
+	# Accounting for invalid input
+	while betAmt > money or betAmt < 0:
+		print("Invalid bet amount (valid amounts are 1-" + str(money) + ").")
+		betAmt = int(input("How much do you want to bet? "))
+
+
+	nums = []
+	
+	# Placing bets
+	print("Enter the numbers:")
+	for i in range(0, choice):
+		bet = input("Number " + str(i + 1) + ": ")
+		
+		# Accounting for invalid input
+		if ((int(bet) < 0 or int(bet) > 36) and bet != "00") or bet in nums:
+			while ((int(bet) < 0 or int(bet) > 36)
+					and bet != "00") or bet in nums:
+				if bet in nums:
+					print(bet + " already used.")
+				print("Please enter a valid roulette wheel number (0-36 and 00).")
+				bet = input("Number " + str(i + 1) + ": ")
+
+	
+		nums.append(bet)
+
+	# Spin wheel
+	result = random.randint(0, 37)
+	
+	print("Result: " + wheel[result])
+
+	# Adjust player's money accordingly
+	if wheel[result] in nums:
+		print("You won!")
+		money += (odds[choice - 1] * betAmt)
+	else:
+		print("You lost!")
+		money -= betAmt
+
+	# Game is over if player runs out of money
+	if money == 0:
+		print("You ran out of money to bet! Game Over.")
+		break
+
+
+	print("Current Balance: $" + str(money))
+
+	choice = int(input("How many numbers do you want to bet on (1-6, 0 to quit)? "))
+
+	while choice < 0 or choice > 6:
+		choice = int(input("Please enter a number 1-6 (0 to quit): "))
+
+
+# Print whether user won or lost money overall
+if money > 10000:
+	print("You made money! You won!!!")
+elif money == 10000:
+	print("You didn't lose money! That's a win in my book.")
+else:
+	print("You lost money! You lose!!!")
+
